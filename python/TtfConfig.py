43c1c66a86e2a7318927bb99646045d9a23396ac
Max Yao <weng.32002@gmail.com>
2020-09-04 00:36:19 +0800

    ver 2.094 convertor

--- python/TtfConfig.py
+++ python/TtfConfig.py
@@ -2,7 +2,7 @@
 #encoding=utf-8
 
 class TtfConfig():
-    VERSION = "2.090"
+    VERSION = "2.094"
     PROCESS_MODE = "GOTHIC"
     PROCESS_MODE = "HALFMOON"
     #PROCESS_MODE = "D"
@@ -17,11 +17,11 @@ class TtfConfig():
     DEFAULT_COORDINATE_VALUE = -9999
 
     # for Regular
-    STROKE_MAX = 99
+    STROKE_MAX = 102
 
     # 有些筆劃很細，有些很粗，設太細似乎又會誤判。
     # 雖然字變重，但因為筆畫複雜時，還是有細的線條。
-    STROKE_MIN = 34
+    STROKE_MIN = 30
 
     # for X,Y axis equal compare.
     # each 100 px, +- 7 px.
@@ -40,6 +40,7 @@ class TtfConfig():
     INSIDE_ROUND_OFFSET = 19
 
     NEED_LOAD_BMP_IMAGE = True
+
     if PROCESS_MODE in ["B2"]:
         NEED_LOAD_BMP_IMAGE = False
         INSIDE_ROUND_OFFSET = 30
@@ -62,6 +63,11 @@ class TtfConfig():
         if STYLE=="Medium":
             INSIDE_ROUND_OFFSET = 30
 
+    # for Regular
+    if PROCESS_MODE in ["TOOTHPASTE"]:
+        OUTSIDE_ROUND_OFFSET = 65
+        INSIDE_ROUND_OFFSET = 25
+
     # some inside block not able to fill 2 curve coner, use small one.
     INSIDE_SMALL_ROUND_OFFSET=15
 
@@ -79,26 +85,26 @@ class TtfConfig():
         if self.STYLE=="Black":
             self.STROKE_MAX = 165
         if self.STYLE=="Bold":
-            self.STROKE_MAX = 137
+            self.STROKE_MAX = 139
         if self.STYLE=="Medium":
-            self.STROKE_MAX = 109
+            self.STROKE_MAX = 115
         if self.STYLE=="DemiLight":
-            self.STROKE_MAX = 86
+            self.STROKE_MAX = 96
         if self.STYLE=="Light":
-            self.STROKE_MAX = 70
+            self.STROKE_MAX = 80
         if self.STYLE=="Thin":
-            self.STROKE_MAX = 49
+            self.STROKE_MAX = 59
 
         if self.STYLE=="Black":
-            self.STROKE_MIN = 36
-        if self.STYLE=="Bold":
-            self.STROKE_MIN = 36
-        if self.STYLE=="Medium":
             self.STROKE_MIN = 34
-        if self.STYLE=="DemiLight":
+        if self.STYLE=="Bold":
+            self.STROKE_MIN = 34
+        if self.STYLE=="Medium":
             self.STROKE_MIN = 30
+        if self.STYLE=="DemiLight":
+            self.STROKE_MIN = 27
         if self.STYLE=="Light":
-            self.STROKE_MIN = 28
+            self.STROKE_MIN = 26
         if self.STYLE=="Thin":
             self.STROKE_MIN = 24
 
@@ -111,17 +117,17 @@ class TtfConfig():
 
 
         if self.STYLE=="Black":
-            self.ROUND_OFFSET = 38
+            self.ROUND_OFFSET = 48
         if self.STYLE=="Bold":
-            self.ROUND_OFFSET = 36
+            self.ROUND_OFFSET = 41
         if self.STYLE=="Medium":
-            self.ROUND_OFFSET = 33
+            self.ROUND_OFFSET = 37
         if self.STYLE=="DemiLight":
             self.ROUND_OFFSET = 30
         if self.STYLE=="Light":
             self.ROUND_OFFSET = 27
         if self.STYLE=="Thin":
-            self.ROUND_OFFSET = 20
+            self.ROUND_OFFSET = 24
 
         if self.STYLE=="Black":
             self.OUTSIDE_ROUND_OFFSET = 65
@@ -134,13 +140,25 @@ class TtfConfig():
         if self.STYLE=="Light":
             self.OUTSIDE_ROUND_OFFSET = 40
         if self.STYLE=="Thin":
-            self.OUTSIDE_ROUND_OFFSET = 28
+            self.OUTSIDE_ROUND_OFFSET = 30
+
+        # bigger curve.
+        if self.PROCESS_MODE in ["TOOTHPASTE"]:
+            if self.STYLE=="Black":
+                self.OUTSIDE_ROUND_OFFSET = 75
+            if self.STYLE=="Bold":
+                self.OUTSIDE_ROUND_OFFSET = 70
+            if self.STYLE=="Medium":
+                self.OUTSIDE_ROUND_OFFSET = 65
+            if self.STYLE=="DemiLight":
+                self.OUTSIDE_ROUND_OFFSET = 60
+            if self.STYLE=="Light":
+                self.OUTSIDE_ROUND_OFFSET = 50
+            if self.STYLE=="Thin":
+                self.OUTSIDE_ROUND_OFFSET = 35
 
         # 只需要大彎.
-        if self.PROCESS_MODE=="HALFMOON":
-            self.ROUND_OFFSET=self.OUTSIDE_ROUND_OFFSET
-
-        if self.PROCESS_MODE in ["D","XD"]:
+        if self.PROCESS_MODE in ["D","XD","HALFMOON","NUT8","TOOTHPASTE"]:
             self.ROUND_OFFSET=self.OUTSIDE_ROUND_OFFSET
 
     def __init__(self, weight_code):
